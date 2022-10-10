66a19695d92184427c467559772e399a58bedb70
Dardan Bakiu <dardan@sogody.com>
2022-10-10 20:20:07 +0200

    mongo db connection

--- app.js
+++ app.js
@@ -19,5 +19,13 @@ app.get('/dir', (req, res) => {
 
 app.use('/api', router);
 
+const mongoUrl = "mongodb+srv://sogody_free_cluster:sgd_free_cluster_pw@cluster0.j3vnx3j.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true, useUnifiedTopology: true
+}, error => {
+  if(error) console.log(error)
+  else console.log('Connected to mongoDB')
+})
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`App running on port : ${PORT}`))
